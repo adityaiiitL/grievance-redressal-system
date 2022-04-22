fde22a38c5e1720eeac99a36c15758ea68a3a1fd
Prabhav Agrawal <prabhavagrawal7@Prabhavs-MacBook-Air.local>
2022-04-22 21:55:21 +0530

    first-commit

---
+++ faculty/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Faculty(models.Model):
+    fid = models.AutoField
+    name = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    contact = models.EmailField(max_length=200) 
+    parent = models.ForeignKey('self', blank = True)
+
+class Complain: 
+    cid = models.AutoField
+    heading = models.CharField(max_length=300)
+    description = models.CharField(max_length=5000)
+    registered_to = models.ForeignKey('Faculty')
+    
