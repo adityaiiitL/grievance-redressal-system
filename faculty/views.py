970ff59a09c5e21281d9c2de928a08f089e0c90c
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2022-04-23 00:24:52 +0530

    views changed 1

--- faculty/views.py
+++ faculty/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from django.db.models import Q
+from django.shortcuts import HttpResponse
+from faculty.models import Faculty, Complain
+from student.models import Student
 # Create your views here.
 def reports(request): 
     return render(request, 'faculty/index.html')
+
+# API's here
+def search(request):
+    if request.method != 'POST':
+        return HttpResponse("<h1> HTTP Method Not Allowed </h1>")
+    else: 
+        query = request.POST.get('query', None)
+        complains = Complain.objects.filter()
--- student/models.py
+++ student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_id = models.AutoField
+    student = models.CharField(max_length=100)
+    branch = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
