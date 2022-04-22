2013748acf963c391930e1e75be412dca952a16a
Prabhav Agrawal <prabhavagrawal7@Prabhavs-MacBook-Air.local>
2022-04-22 22:34:02 +0530

    everything

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,3 +1,6 @@
+import imp
 from django.contrib import admin
-
+from .models import Faculty,Complain
 # Register your models here.
+admin.site.register(Faculty)
+admin.site.register(Complain)
--- faculty/models.py
+++ faculty/models.py
@@ -6,11 +6,11 @@ class Faculty(models.Model):
     name = models.CharField(max_length=100)
     designation = models.CharField(max_length=100)
     contact = models.EmailField(max_length=200) 
-    parent = models.ForeignKey('self', blank = True)
+    parent = models.ForeignKey('self', blank = True, on_delete=models.CASCADE)
 
-class Complain: 
+class Complain(models.Model): 
     cid = models.AutoField
     heading = models.CharField(max_length=300)
     description = models.CharField(max_length=5000)
-    registered_to = models.ForeignKey('Faculty')
-    
+    registered_to = models.ForeignKey('faculty', on_delete=models.CASCADE)
+
