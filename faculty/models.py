1976230bd03971b915c05f2a471590519c366ed7
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2022-04-23 00:49:29 +0530

    changes

--- faculty/models.py
+++ faculty/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Faculty(models.Model):
-    fid = models.AutoField
+    faculty_id = models.AutoField
     name = models.CharField(max_length=100)
     designation = models.CharField(max_length=100)
     contact = models.EmailField(max_length=200) 
     parent = models.ForeignKey('self', blank = True, on_delete=models.CASCADE)
 
 class Complain(models.Model): 
-    cid = models.AutoField
+    complain_id = models.AutoField
     heading = models.CharField(max_length=300)
     description = models.CharField(max_length=5000)
     registered_to = models.ForeignKey('faculty', on_delete=models.CASCADE)
