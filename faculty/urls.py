9bf561ff5c90ea2e7730c8eae2b58e90231be364
akanksha <lit2020026@gmail.com>
2022-04-23 15:06:54 +0530

    add

--- faculty/urls.py
+++ faculty/urls.py
@@ -4,11 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('report/', views.report, name="report"),
     path('read/<int:id>', views.read, name='read'),
-=======
-    path('reports/', views.reports, name="reports"),
-    path('complain/', views.Complain_, name="complain"),
->>>>>>> 90957059a6bfcda9ecef85c21ca00fd55cb0ea87
 ]
