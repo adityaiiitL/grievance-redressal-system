e4d56d81b38898ae91808c5d083244b43a012563
Tj8864 <lit2020025@iiitl.ac.in>
2022-11-30 18:04:00 +0530

    ChangeS

--- faculty/urls.py
+++ faculty/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('report/', views.report, name="report"),
     path('read/<slug:complain_id>', views.read, name='read'),
+    path('elevate/<slug:complain_id>', views.elevateComplain, name='elevateComplain'),
+    path('complete/<slug:complain_id>', views.completeComplain, name='completeComplain'),
     path('complainform/', views.complainform, name='complainform')
 ]
